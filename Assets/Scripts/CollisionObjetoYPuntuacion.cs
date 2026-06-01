d83a31115c7805a47a8eeee612266a2bc44e7842
MatiCABJ12 <50233785@est.ort.edu.ar>
2026-06-01 14:25:20 -0300

    listo pasillo, listo el celu y listo el collision con los puntos(hay que desarrollarlo)

---
+++ Assets/Scripts/CollisionObjetoYPuntuacion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionObjetoYPuntuacion : MonoBehaviour
+{
+    private int puntaje = 0;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Coleccionable"))
+        {
+            puntaje++;
+            Debug.Log("¡Objeto encontrado! Puntaje: " + puntaje);
+            Destroy(other.gameObject);
+        }
+    }
+}
