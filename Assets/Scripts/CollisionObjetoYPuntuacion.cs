857c6f7866a887433ab8652572d533f4332fc60e
MatiCABJ12 <50233785@est.ort.edu.ar>
2026-06-08 14:21:49 -0300

    a ver

--- Assets/Scripts/CollisionObjetoYPuntuacion.cs
+++ Assets/Scripts/CollisionObjetoYPuntuacion.cs
@@ -2,13 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class CollisionObjetoYPuntuacion : MonoBehaviour
 {
+    public int recolectados = 0;
+    private UIManager uiManagerScript;
+    
+     void Start()
+    {
+        uiManagerScript = FindObjectOfType<UIManager>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Coleccionable"))
         {
             Destroy(other.gameObject);
+            recolectados = recolectados + 1;
+            uiManagerScript.UpdatePuntaje(recolectados);
         }
     }
 }
